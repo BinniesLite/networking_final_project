83737b43249ebc917b6dc8da2c5929cdbe46ec18
qme02 <108671784+qme02@users.noreply.github.com>
2023-11-20 12:38:32 -0500

    Create client.py

---
+++ client.py
@@ -0,0 +1,40 @@
+import socket
+import threading
+
+def receive_messages():
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+            print(message)
+        except:
+            print('An error occurred!')
+            client.close()
+            break
+
+def send_messages():
+    while True:
+        try:
+            command = input()
+            
+            # Send commands to the server
+            client.send(command.encode('utf-8'))
+            
+        except:
+            print('An error occurred!')
+            client.close()
+            break
+
+if __name__ == "__main__":
+
+    host = 'localhost'
+    port = 6789
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host, port))
+    
+    # Start receiving messages in a separate thread
+    receive_thread = threading.Thread(target=receive_messages)
+    receive_thread.start()
+    
+    # Start sending messages (commands) in the main thread
+    send_thread = threading.Thread(target=send_messages)
+    send_thread.start()
